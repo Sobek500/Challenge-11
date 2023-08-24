1fa9d829e015cf59f6ba1b31e673c38fe1ddb865
Sobek500 <113941426+Sobek500@users.noreply.github.com>
2023-08-24 18:53:39 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
-
-const notes = require('./notes');
-const apiNotes = require('./api/notes');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use('/notes', notes);
-app.use('/api/notes', apiNotes);
+app.use(express.json());
 
-module.exports = app;
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.get('/api/notes', (req, res) => {
+  const dbPath = path.join(__dirname, '..', 'db', 'db.json');
+  const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+  res.json(data);
+});
+
+app.post('/api/notes', (req, res) => {
+  const dbPath = path.join(__dirname, '..', 'db', 'db.json');
+  const data = JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+
+  const newNote = {
+    id: Date.now().toString(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  data.push(newNote);
+
+  fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+
+  res.json(newNote);
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
